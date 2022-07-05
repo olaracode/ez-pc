a1530ef6c8bdeb192b1e01e38ec0bf0e6c584f9b
Brian Marquez <brnmarq@BrnMarq>
2022-07-04 23:36:40 -0400

    test link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
 import Todo from "@/organisms/todo/Todo";
 import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <Box mx={"6%"} my={"2%"}>
-      <Todo />
-    </Box>
-  );
+	return (
+		<Box mx={"6%"} my={"2%"}>
+			<Todo />
+			<Link href='/test/hola-mundo'>Test link</Link>
+		</Box>
+	);
 }
